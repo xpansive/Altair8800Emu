b7b7dbb1d6ec782fd449f9b5ae5b36b2a9439a56
xpansive <xpansive@live.ca>
2011-12-10 16:13:21 -0700

    Some formatting/comment fixes

--- src/com/xpansive/altair8800/Altair8800.java
+++ src/com/xpansive/altair8800/Altair8800.java
@@ -14,10 +14,9 @@ public class Altair8800 {
         CPU proc = new CPU();
         int memory[] = new int[] { 
         		0x3A, 0x08, 0x00, 	//LDA 0x0008
-                0x47,				//MOV B,A
-                0x80,				//ADD B
-                0xC3, 0x04, 0x00, 	//JMP 0x0000
-                0x01
+                0xC6, 0x05,         //ADI 0x05
+                0xC3, 0x03, 0x00, 	//JMP 0x0000
+                0x00
         };
         proc.getMemory().writeBytes(0, memory);
         Scanner s = new Scanner(System.in);
@@ -67,16 +66,17 @@ public class Altair8800 {
         sb.append(padHex(memLocation, 4)); // Memory location
         sb.append("\t" + op.getName(mem.readByte(memLocation)) + "       "); // Opcode name
         switch (op.getLength()) {
-        case 2:
-            sb.append(padHex(mem.readByte(memLocation + 1), 2)); //single byte argument
-            break;
         case 3:
             sb.append(padHex(mem.readByte(memLocation + 1) | (mem.readByte(memLocation + 1) << 8), 4)); //word argument
             break;
+        case 2:
+            sb.append(padHex(mem.readByte(memLocation + 1), 2)); //single byte argument
+            //Fallthrough to append an extra tab
         default:
             sb.append("\t");
                 break;
         }
+        // Add in register values
         sb.append("\tA=" + cpu.getRegisterValue(Register.A));
         sb.append("\tB=" + cpu.getRegisterValue(Register.B));
         sb.append("\tC=" + cpu.getRegisterValue(Register.C));
@@ -85,13 +85,17 @@ public class Altair8800 {
         sb.append("\tH=" + cpu.getRegisterValue(Register.H));
         sb.append("\tL=" + cpu.getRegisterValue(Register.L));
         sb.append("\tSP=" + cpu.getRegisterValue(Register.SP));
+
+        //Flags
         sb.append("\tF=" + padBin(cpu.getFlags(), 8));
+
         return sb.toString();
     }
 
     private static String padHex(int num, int pad) {
         return "0x" + String.format("%" + pad + "s", Integer.toHexString(num)).replace(' ', '0');
     }
+
     private static String padBin(int num, int pad) {
         return "0b" + String.format("%" + pad + "s", Integer.toBinaryString(num)).replace(' ', '0');
     }
