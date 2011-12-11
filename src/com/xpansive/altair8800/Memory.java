b1dc527863c2175465b490e98f1ba0d41cc4e235
xpansive <xpansive@live.ca>
2011-12-11 15:25:41 -0700

    Memory bounds checking

--- src/com/xpansive/altair8800/Memory.java
+++ src/com/xpansive/altair8800/Memory.java
@@ -2,35 +2,48 @@ package com.xpansive.altair8800;
 
 public class Memory {
     private int[] memory;
+    private int size;
 
     public Memory(int size) {
         memory = new int[size];
+        this.size = size;
     }
 
     public int readByte(int loc) {
-        return memory[loc];
+        if (loc <= size)
+            return memory[loc];
+        return 0;
     }
 
-    public void writeByte(int loc, int val) {
-        memory[loc] = val;
+    public void writeByte(int loc, byte val) {
+        if (loc <= size)
+            memory[loc] = val;
     }
 
     public int readWord(int loc) {
-        return memory[loc] | memory[loc + 1] << 8;
+        if (loc + 1 <= size)
+            return memory[loc] | memory[loc + 1] << 8;
+        return 0;
     }
 
     public void writeWord(int loc, int val) {
-        memory[loc] = val & 0xFF;
-        memory[loc + 1] = val >> 8;
+        if (loc + 1 <= size) {
+            memory[loc] = (byte) (val & 0xFF);
+            memory[loc + 1] = (byte) (val >> 8);
+        }
     }
 
     public int[] readBytes(int loc, int num) {
-        int[] temp = new int[num];
-        System.arraycopy(memory, loc, temp, 0, num);
-        return temp;
+        if (loc + num <= size) {
+            int[] temp = new int[num];
+            System.arraycopy(memory, loc, temp, 0, num);
+            return temp;
+        }
+        return new int[0];
     }
 
     public void writeBytes(int loc, int[] values) {
-        System.arraycopy(values, 0, memory, loc, values.length);
+        if (loc + values.length <= size)
+            System.arraycopy(values, 0, memory, loc, values.length);
     }
 }
